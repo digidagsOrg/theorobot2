e03a304477c9a79abf4d3fa71434f2229d277adc
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-06-24 15:58:21 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,33 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 13) {
-        Position2 = ServoSlow.setServoAngle(2, 70, 10, Position2, ServoSlow.ServoDirection.CW)
-        Position2 = ServoSlow.setServoAngle(2, 70, 10, Position2, ServoSlow.ServoDirection.CW)
-        basic.pause(500)
-        Position2 = ServoSlow.setServoAngle(2, 140, 10, Position2, ServoSlow.ServoDirection.CCW)
-        basic.pause(500)
-        Position2 = ServoSlow.setServoAngle(2, 70, 10, Position2, ServoSlow.ServoDirection.CW)
-        basic.pause(500)
-        position1 = ServoSlow.setServoAngle(1, 20, 10, position1, ServoSlow.ServoDirection.CW)
-        basic.pause(500)
-        position1 = ServoSlow.setServoAngle(1, 40, 10, position1, ServoSlow.ServoDirection.CCW)
-        basic.pause(500)
-        position1 = ServoSlow.setServoAngle(1, 20, 10, position1, ServoSlow.ServoDirection.CW)
+    if (receivedNumber == 15) {
+        motion1()
+    } else if (receivedNumber == 16) {
+        motion2()
     } else if (receivedNumber == 0) {
-        // oben
-        kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo1, 110)
-        // oben
-        kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo2, 100)
-        position1 = 110
-        Position2 = 100
+        Ausgangsposition()
     }
 })
 input.onButtonPressed(Button.A, function () {
+    motion1()
+})
+function motion2 () {
+	
+}
+input.onButtonPressed(Button.AB, function () {
+    Ausgangsposition()
+})
+input.onButtonPressed(Button.B, function () {
+    motion2()
+})
+function Ausgangsposition () {
+    // servoOben
+    kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo1, 110)
+    // unten
+    kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo2, 100)
+    position1 = 110
+    Position2 = 100
+}
+function motion1 () {
     Position2 = ServoSlow.setServoAngle(2, 70, 10, Position2, ServoSlow.ServoDirection.CW)
     Position2 = ServoSlow.setServoAngle(2, 70, 10, Position2, ServoSlow.ServoDirection.CW)
     basic.pause(500)
@@ -34,20 +40,8 @@ input.onButtonPressed(Button.A, function () {
     position1 = ServoSlow.setServoAngle(1, 40, 10, position1, ServoSlow.ServoDirection.CCW)
     basic.pause(500)
     position1 = ServoSlow.setServoAngle(1, 20, 10, position1, ServoSlow.ServoDirection.CW)
-})
-input.onButtonPressed(Button.AB, function () {
-    // oben
-    kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo1, 110)
-    // oben
-    kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo2, 100)
-    position1 = 110
-    Position2 = 100
-})
+}
 let Position2 = 0
 let position1 = 0
-// servoOben
-kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo1, 110)
-// unten
-kitronik_simple_servo.setServoAngle(kitronik_simple_servo.ServoChoice.servo2, 100)
-position1 = 110
-Position2 = 100
+radio.setGroup(1)
+Ausgangsposition()
